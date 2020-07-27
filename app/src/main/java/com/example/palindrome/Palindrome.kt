25bbcfb655e5144fd9c8a9493ec1577c5d9055e0
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-27 10:10:27 +0100

    keep only digits and letters from the string and initialize a counter

--- app/src/main/java/com/example/palindrome/Palindrome.kt
+++ app/src/main/java/com/example/palindrome/Palindrome.kt
@@ -6,6 +6,7 @@ fun main(args:Array<String>){
 }
 
 fun checkPalindrome(wordString:String){
-
-
+    //keep only letter or digit in the string and take out all symbols
+    val wordStrFiltered=wordString.filter { it.isLetterOrDigit() }
+    var counter=0
 }
