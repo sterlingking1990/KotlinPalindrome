155bf3307d2e878c275f7882cfa07b6961468350
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-27 10:12:56 +0100

    loop filtered string in reverse upto the middle of the string, comparing each with origin string

--- app/src/main/java/com/example/palindrome/Palindrome.kt
+++ app/src/main/java/com/example/palindrome/Palindrome.kt
@@ -9,4 +9,13 @@ fun checkPalindrome(wordString:String){
     //keep only letter or digit in the string and take out all symbols
     val wordStrFiltered=wordString.filter { it.isLetterOrDigit() }
     var counter=0
+    for (i in wordStrFiltered.length-1 downTo wordStrFiltered.length/2){
+        var frontCount=(wordString.length-1)-i
+        if (wordStrFiltered[frontCount]==wordString[i]){
+            counter+=1
+        }
+        else{
+            counter=0
+        }
+    }
 }
